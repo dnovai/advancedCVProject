0bb25304ce994ba6d05244faea662513603d18b2
dnovai <d.nova.inostroza@gmail.com>
2022-02-21 21:58:45 -0300

    adding patterns

--- finger_counting.py
+++ finger_counting.py
@@ -2,19 +2,24 @@ import cv2
 import os
 import time
 import advancedcv.hand_tracking as htm
+import numpy as np
+import itertools
+
+patterns = np.array(list(itertools.product([0, 1], repeat=5)))
 
 p_time = 0
 cap = cv2.VideoCapture(0)
 
 folder_path = "finger_images"
 my_list = os.listdir(folder_path)
+my_list.sort()
 overlay_list = []
 
 detector = htm.HandDetector()
 
 for im_path in my_list:
     image = cv2.imread(f'{folder_path}/{im_path}')
-    print(f'{folder_path}/{im_path}')
+    # print(f'{folder_path}/{im_path}')
     overlay_list.append(image)
 
 key_ids = [4, 8, 12, 16, 20]
@@ -40,17 +45,22 @@ while True:
             else:
                 fingers.append(0)
 
-        total_fingers = fingers.count(1)
+        dist = (patterns - fingers)**2
+        dist = np.sum(dist, axis=1)
+        min_value = min(dist)
+        min_index = dist.index(min_value)
 
-        print(total_fingers, overlay_list[total_fingers+1])
-        h, w, c = overlay_list[total_fingers+1].shape
-        img[0:h, 0:w] = overlay_list[total_fingers+1]
+        # total_fingers = fingers.count(1)
+        #
+        # print(total_fingers, overlay_list[total_fingers+1])
+        # h, w, c = overlay_list[total_fingers+1].shape
+        # img[0:h, 0:w] = overlay_list[total_fingers+1]
     
     c_time = time.time()
     fps = 1/(c_time-p_time)
     p_time = c_time
     
-    cv2.putText(img, f'FPS: {str(round(fps))}', (400, 70), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 0), 3)
-    cv2.imshow("Image", img)
+    # cv2.putText(img, f'FPS: {str(round(fps))}', (400, 70), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 0), 3)
+    # cv2.imshow("Image", img)
     cv2.waitKey(1)
     
